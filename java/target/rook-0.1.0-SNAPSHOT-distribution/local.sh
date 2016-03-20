43419506555f0629417df682c3a1ebf88f8fbbe3
Eric Thill <ejthill@gmail.com>
2016-03-20 17:07:32 -0500

    Cleanup

--- java/target/rook-0.1.0-SNAPSHOT-distribution/local.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -cp "api/*" rook.api.RookRunner $1
--- java/target/rook-0.1.0-SNAPSHOT-distribution/ui.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#java -cp "ui/*" rook.ui.UIRunner --html ../../ui/html --port 9000
-java -cp "ui/*" rook.ui.UIRunner --html ui/html --port 9000
