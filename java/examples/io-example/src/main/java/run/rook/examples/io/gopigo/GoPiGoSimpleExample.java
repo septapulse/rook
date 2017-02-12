476a9e0497d3d24303d10bef87d7783e69cb9cb9
Eric Thill <ejthill@gmail.com>
2017-02-12 14:10:42 -0600

    GoPiGoSimpleExample

--- java/examples/io-example/src/main/java/run/rook/examples/io/gopigo/GoPiGoSimpleExample.java
+++ java/examples/io-example/src/main/java/run/rook/examples/io/gopigo/GoPiGoSimpleExample.java
@@ -5,101 +5,65 @@ import org.slf4j.LoggerFactory;
 
 import run.rook.api.RID;
 import run.rook.api.Service;
-import run.rook.api.config.Configurable;
+import run.rook.api.ServiceLauncher;
 import run.rook.api.exception.InitException;
 import run.rook.api.transport.Transport;
-import run.rook.api.util.Sleep;
 import run.rook.core.io.proxy.IOProxy;
+import run.rook.core.io.proxy.message.IOValue;
 
 /**
- * If you get too close to the GoPiGo's ultrasonic distance sensor, you'll scare it backwards.
+ * If you get too close to the GoPiGo's ultrasonic distance sensor, the LED's will light up.
  */
 public class GoPiGoSimpleExample implements Service {
 
-	private final Logger logger = LoggerFactory.getLogger(getClass());
+	private static final String MY_ROBOT_IP = "192.168.99.3";
 	
-	// parsed from the configuration
-	private final int escapeDistance;
-	private final RID goPiGoServiceId;
-	
-	// create during init
-	private IOProxy ioProxy;
-	private MyGoPiGo robot;
-
-	// marker to tell our outputLoop to keep running
-	private volatile boolean run = true;
-	
-	@Configurable
-	public GoPiGoSimpleExample(GoPiGoExampleConfig config) {
-		// parse configuration values
-		// note: the configuration defines defaults for everything
-		this.escapeDistance = config.escapeDistance;
-		this.goPiGoServiceId = new RID(config.goPiGoServiceId);
+	public static void main(String[] args) {
+		ServiceLauncher.main(
+				"-id", "MY_SERVICE",
+				"-st", "run.rook.examples.io.gopigo.GoPiGoSimpleExample",
+				"-sc", "{}",
+				"-tt", "run.rook.core.transport.websocket.WebsocketTransport",
+				"-tc", "{\"url\":\"ws://"+MY_ROBOT_IP+":8080\"}");
 	}
 	
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	// ID's of inputs and outputs
+	private final RID ledIdLeft = new RID("LED_LEFT");
+	private final RID ledIdRight = new RID("LED_RIGHT");
+	private final RID distanceId = new RID("DISTANCE");
+	
+	// create using the Transport
+	private IOProxy ioProxy;
+	
+	// keep track of the current LED Status
+	private boolean curLedStatus = false;
+
 	@Override
 	public void setTransport(Transport transport) {
-		// Create the IOProxy
-		ioProxy = new IOProxy(transport, goPiGoServiceId);
+		// Create the IOProxy to the "IO" service, which takes care of all underlying communication.
+		ioProxy = new IOProxy(transport, new RID("IO"));
 	}
 
 	@Override
 	public void init() throws InitException {
-		// Create MyGoPiGo robot instance
-		robot = new MyGoPiGo(ioProxy);
-		// Start control thread
-		new Thread(this::controlLoop).start();
-		// Start log thread
-		new Thread(this::logLoop).start();
+		// Listen for "distance" updates
+		ioProxy.inputs().addFilteringConsumer(distanceId, this::handleDistance);
 	}
 	
-	private void controlLoop() {
-		// loop logic
-		while(run) {
-			// Don't kill the processor
-			Sleep.trySleep(20);
-			
-			// Check if the distance sensor reports a value that meets our threshold
-			long distance = robot.getDistance();
-			if(distance <= escapeDistance) {
-				logger.info("Distance: " + distance);
-				logger.info("Ah! Too close! Backing up...");
-				
-				// start backing away
-				robot.setLedLeft(true);
-				robot.setLedRight(true);
-				robot.setMotors(-150, -150);
-				
-				// wait one second
-				Sleep.trySleep(1000);
-				
-				// stop backing away
-				robot.setMotors(0, 0);
-				robot.setLedLeft(false);
-				robot.setLedRight(false);
-				Sleep.trySleep(1000);
-				
-				// log "Phew!" if we're no longer within our escape distance threshold
-				distance = robot.getDistance();
-				if(distance > escapeDistance) {
-					logger.info("Distance: " + distance);
-					logger.info("Phew!");
-				}
-			}
-		}
-	}
-	
-	private void logLoop() {
-		// loop logic
-		while(run) {
-			Sleep.trySleep(3000);
-			logger.info("Distance: " + robot.getDistance());
+	void handleDistance(RID id, IOValue value) {
+		logger.info("Distance is " + value);
+		boolean newLedStatus = value.getValueAsInt() < 30;
+		if(newLedStatus != curLedStatus) {
+			ioProxy.outputs().setOutput(ledIdLeft, new IOValue(newLedStatus));
+			ioProxy.outputs().setOutput(ledIdRight, new IOValue(newLedStatus));
+			curLedStatus = newLedStatus;
 		}
 	}
 	
 	@Override
 	public void shutdown() {
-		run = false;
 		ioProxy.stop();
 	}
 
